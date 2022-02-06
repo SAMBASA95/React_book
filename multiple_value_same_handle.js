710d110175a8903d2e27edce397382d064e52ac3
swapnilukey1995 <83223911+swapnilukey1995@users.noreply.github.com>
2022-02-06 19:41:07 +0530

    Update multiple_value_same_handle.js

--- multiple_value_same_handle.js
+++ multiple_value_same_handle.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const ControlledInputs = () => {
   // const [firstName, setFirstName] = useState('')
   // const [email, setEmail] = useState('')
   // const [age, setAge] = useState('')
+  const refContainer = useRef(null)
   const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
   const [people, setPeople] = useState([])
 
@@ -12,7 +13,14 @@ const ControlledInputs = () => {
     const value = e.target.value
     // console.log(name, value);
     setPerson({ ...person, [name]: value })
+    if (person.firstName && person.email && person.age === '') {
+      console.log('found All')
+    } else {
+      console.log('No!!')
+    }
   }
+  // console.log(people);
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (person.firstName && person.email && person.age) {
@@ -21,6 +29,12 @@ const ControlledInputs = () => {
       setPerson({ firstName: '', email: '', age: '' })
     }
   }
+
+  useEffect(() => {
+    // console.log(refContainer.current.value)
+    // console.log(refContainer.current)
+    refContainer.current.focus()
+  })
 
   return (
     <>
@@ -32,6 +46,7 @@ const ControlledInputs = () => {
               type="text"
               id="firstName"
               name="firstName"
+              ref={refContainer}
               value={person.firstName}
               onChange={handleChange} />
           </div>
@@ -57,7 +72,7 @@ const ControlledInputs = () => {
         </form>
         {
           people.map((person) => {
-            const { id, firstName, email, age } = person
+            const { id } = person
             return (
               <div className="item" key={id}>
                 <h4>{person.firstName}</h4>
